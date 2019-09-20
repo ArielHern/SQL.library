3db6029bcee15868da29f4b000de3cdc4145a008
Ariel Hern <hernandez.ariel1228@gmail.com>
2019-09-20 10:11:01 -0400

    added test for update

--- test_procedures.sql
+++ test_procedures.sql
@@ -8,12 +8,17 @@ DECLARE
     l_author VARCHAR2(200) := 'Ari';
     l_published_date DATE := TO_DATE('19-SEP-2019','DD-MM-YYYY');   
     l_total_pages NUMBER := 1; 
+	
+	l_title_update VARCHAR2(200) := 'Title updated';
 
     CURSOR bookcountCur IS 
         SELECT COUNT(*) FROM books;
     
-    books_count NUMBER;
+    
+	books_count NUMBER;
 	l_sqlcode NUMBER;
+	
+	
        
 BEGIN
     DELETE books;
@@ -26,7 +31,7 @@ BEGIN
     FETCH bookcountCur INTO books_count;
 	
 	--test report.pro	
-    report('Check the total of books: ', expected_value => '1', actual_value => books_count);
+    report('Procedure add_books: ', expected_value => '1', actual_value => books_count);
     CLOSE bookcountCur;
 	
 	--duplicate
@@ -37,7 +42,14 @@ BEGIN
 		WHEN OTHERS THEN
 			l_sqlcode := SQLCODE;
 	END;
-	report('detention of duplicate ISBN number : ', expected_value => '-1', actual_value => l_sqlcode);
+	report('Detention of duplicate ISBN number : ', expected_value => '-1', actual_value => l_sqlcode);
+	
+	--update book
+	BEGIN
+		update_book(1111111111,l_title_update,'Ari',TO_DATE('19-SEP-2019', 'DD-MM-YYYY'), 1);
+	END;
+	report('Procedure update_book: ', expected_value=>'Title updated', actual_value=>l_title_update);
+	
 	
 	
    
