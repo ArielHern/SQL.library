1c3b21a84615d49ca5d1a2a7e448c5ed09dac550
Ariel Hern <hernandez.ariel1228@gmail.com>
2019-09-20 10:09:27 -0400

    more test added to report

--- report_pro.sql
+++ report_pro.sql
@@ -9,14 +9,13 @@ CREATE OR REPLACE PROCEDURE report(
 	expected_value IN VARCHAR2,
 	actual_value IN VARCHAR2)
 AS 
-BEGIN 
-	DBMS_OUTPUT.PUT(test_title || ': ');
+BEGIN	
 	IF expected_value = actual_value
 	OR (expected_value IS NULL AND actual_value IS NULL)
 	THEN 
-		DBMS_OUTPUT.PUT('PASSED');
+		DBMS_OUTPUT.PUT_LINE(test_title || 'PASSED ');
 	ELSE
-		DBMS_OUTPUT.PUT('FAILED. Expected: ' || expected_value || 'got '|| actual_value);
+		DBMS_OUTPUT.PUT_LINE('FAILED. Expected: ' || expected_value || 'got '|| actual_value);
 	END IF;
-END test_report  
+END report  
 ;
